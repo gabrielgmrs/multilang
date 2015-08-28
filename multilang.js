5639cab8242ff9da1e596b7e6ac229aa3c75857a
Gabriel Guimarães <gabriel.guims@gmail.com>
2015-08-28 14:18:57 -0300

    Separeted .js file

---
+++ multilang.js
@@ -0,0 +1,23 @@
+function printLang(theLang) {
+	$.ajax({
+	    url: 'languages.xml',
+	    success: function(xml) {
+	        $(xml).find('translation').each(function(){
+	            var id = $(this).attr('id');
+	            var text = $(this).find(theLang).html();
+	            $('.'+id).html(text);
+	        });
+	    }
+	});
+};
+$(document).ready(function() {
+	printLang('pt');
+});
+$(document).on('click', 'button.language', function(event) {
+	event.preventDefault();
+	if ($(this).attr('lang') == 'pt') {
+		printLang('pt');
+	} else if ($(this).attr('lang') == 'en') {
+		printLang('en');
+	}
+});
